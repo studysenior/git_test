2e2f6c926a5ced0be4e85f4d54fc5c325280f599
newlysyl1107 <official.multifidus@gmail.com>
2022-02-24 16:14:21 +0900

    test

--- src/main/java/com/example/git_test/TestClass.java
+++ src/main/java/com/example/git_test/TestClass.java
@@ -1,8 +1,12 @@
 package com.example.git_test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestClass {
 
 	// 테스트 클래스
 	// ㅁㅇㄴㄹㅇㄴㄹㄴㄹ
 	// ㅇㄹㅇㄴㅁㄹㅇㄴㄹㄴㅇㄹㅇㄴㄹㅇㄴ
+	
+	//
 }
